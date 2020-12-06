ef235cfdbf57bfe6c35f3bc8c9e063b8c2720903
Nathan <nathan@nr5p.com>
2020-12-06 09:25:23 -0600

    pointcloud showing

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
 import pyrealsense2.pyrealsense2 as rs
 import numpy as np
 import cv2, time
+from appState import AppState
 
 class Display():
     def __init__(self):
@@ -13,6 +14,8 @@ class Display():
         self.lastImg = None
         self.rectangleStarted = False
         self.clipping_distance = 1 # clipping distance in meters
+        self.state = AppState()
+        self.pointCloudImage = np.zeros((self.VID_HEIGHT,self.VID_WIDTH,3), np.uint8)
 
         # Create a pipeline
         self.pipeline = rs.pipeline()
@@ -40,6 +43,18 @@ class Display():
 
         cv2.namedWindow("chute", cv2.WINDOW_AUTOSIZE)
         cv2.setMouseCallback("chute",self.process_click)
+
+        # Get stream profile and camera intrinsics
+        profile = self.pipeline.get_active_profile()
+        depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+        self.depth_intrinsics = depth_profile.get_intrinsics()
+        self.w, self.h = self.depth_intrinsics.width, self.depth_intrinsics.height
+
+        # Processing blocks
+        self.pc = rs.pointcloud()
+        decimate = rs.decimation_filter()
+        decimate.set_option(rs.option.filter_magnitude, 2 ** self.state.decimate)
+        self.colorizer = rs.colorizer()
 
 
     def createButtons(self):
@@ -129,17 +144,179 @@ class Display():
 
         return preproc_img
 
-    def display(self, image, bgRemoved, depthImage):
+    def display(self, image, bgRemoved, depthImage, pointCloudImg):
         blankImage = np.zeros((self.VID_HEIGHT,self.VID_WIDTH,3), np.uint8)
         blankImageButtons = np.zeros((self.VID_HEIGHT,200,3), np.uint8)
         imgWithButtons = np.hstack((image,bgRemoved,self.buttonImg))
-        bottomImages = np.hstack((depthImage,blankImage,blankImageButtons))
+        bottomImages = np.hstack((depthImage,pointCloudImg,blankImageButtons))
         entireScreen = np.vstack((imgWithButtons, bottomImages))
         if self.captureAreaBox[0] != 0 and self.captureAreaBox[1] != 0:
             cv2.rectangle(entireScreen, self.captureAreaBox[0], self.captureAreaBox[1], (0,255,0), thickness=1) 
         if self.triggerAreaBox[0] != 0 and self.triggerAreaBox[1] != 0:
             cv2.rectangle(entireScreen, self.triggerAreaBox[0], self.triggerAreaBox[1], (0,0,255), thickness=1) 
         cv2.imshow("chute", entireScreen)
+
+    
+    def project(self,v):
+        """project 3d vector array to 2d"""
+        h, w = self.pointCloudImage.shape[:2]
+        view_aspect = float(h)/w
+
+        # ignore divide by zero for invalid depth
+        with np.errstate(divide='ignore', invalid='ignore'):
+            proj = v[:, :-1] / v[:, -1, np.newaxis] * \
+                (w*view_aspect, h) + (w/2.0, h/2.0)
+
+        # near clipping
+        znear = 0.03
+        proj[v[:, 2] < znear] = np.nan
+        return proj
+
+
+    def view(self,v):
+        """apply view transformation on vector array"""
+        return np.dot(v - self.state.pivot, self.state.rotation) + self.state.pivot - self.state.translation
+
+
+    def line3d(self, out, pt1, pt2, color=(0x80, 0x80, 0x80), thickness=1):
+        """draw a 3d line from pt1 to pt2"""
+        p0 = self.project(pt1.reshape(-1, 3))[0]
+        p1 = self.project(pt2.reshape(-1, 3))[0]
+        if np.isnan(p0).any() or np.isnan(p1).any():
+            return
+        p0 = tuple(p0.astype(int))
+        p1 = tuple(p1.astype(int))
+        rect = (0, 0, out.shape[1], out.shape[0])
+        inside, p0, p1 = cv2.clipLine(rect, p0, p1)
+        if inside:
+            cv2.line(out, p0, p1, color, thickness, cv2.LINE_AA)
+
+
+    def grid(self, out, pos, rotation=np.eye(3), size=1, n=10, color=(0x80, 0x80, 0x80)):
+        """draw a grid on xz plane"""
+        pos = np.array(pos)
+        s = size / float(n)
+        s2 = 0.5 * size
+        for i in range(0, n+1):
+            x = -s2 + i*s
+            self.line3d(out, self.view(pos + np.dot((x, 0, -s2), rotation)),
+                self.view(pos + np.dot((x, 0, s2), rotation)), color)
+        for i in range(0, n+1):
+            z = -s2 + i*s
+            self.line3d(out, self.view(pos + np.dot((-s2, 0, z), rotation)),
+                self.view(pos + np.dot((s2, 0, z), rotation)), color)
+
+
+    def axes(self, out, pos, rotation=np.eye(3), size=0.075, thickness=2):
+        """draw 3d axes"""
+        self.line3d(out, pos, pos +
+            np.dot((0, 0, size), rotation), (0xff, 0, 0), thickness)
+        self.line3d(out, pos, pos +
+            np.dot((0, size, 0), rotation), (0, 0xff, 0), thickness)
+        self.line3d(out, pos, pos +
+            np.dot((size, 0, 0), rotation), (0, 0, 0xff), thickness)
+
+
+    def frustum(self, out, intrinsics, color=(0x40, 0x40, 0x40)):
+        """draw camera's frustum"""
+        orig = self.view([0, 0, 0])
+        w, h = intrinsics.width, intrinsics.height
+
+        for d in range(1, 6, 2):
+            def get_point(x, y):
+                p = rs.rs2_deproject_pixel_to_point(intrinsics, [x, y], d)
+                self.line3d(out, orig, self.view(p), color)
+                return p
+
+            top_left = get_point(0, 0)
+            top_right = get_point(w, 0)
+            bottom_right = get_point(w, h)
+            bottom_left = get_point(0, h)
+
+            self.line3d(out, self.view(top_left), self.view(top_right), color)
+            self.line3d(out, self.view(top_right), self.view(bottom_right), color)
+            self.line3d(out, self.view(bottom_right), self.view(bottom_left), color)
+            self.line3d(out, self.view(bottom_left), self.view(top_left), color)
+
+
+    def pointcloud(self, out, verts, texcoords, color, painter=True):
+        """draw point cloud with optional painter's algorithm"""
+        if painter:
+            # Painter's algo, sort points from back to front
+
+            # get reverse sorted indices by z (in view-space)
+            # https://gist.github.com/stevenvo/e3dad127598842459b68
+            v = self.view(verts)
+            s = v[:, 2].argsort()[::-1]
+            proj = self.project(v[s])
+        else:
+            proj = self.project(view(verts))
+
+        if self.state.scale:
+            proj *= 0.5**self.state.decimate
+
+        h, w = out.shape[:2]
+
+        # proj now contains 2d image coordinates
+        j, i = proj.astype(np.uint32).T
+
+        # create a mask to ignore out-of-bound indices
+        im = (i >= 0) & (i < h)
+        jm = (j >= 0) & (j < w)
+        m = im & jm
+
+        cw, ch = color.shape[:2][::-1]
+        if painter:
+            # sort texcoord with same indices as above
+            # texcoords are [0..1] and relative to top-left pixel corner,
+            # multiply by size and add 0.5 to center
+            v, u = (texcoords[s] * (cw, ch) + 0.5).astype(np.uint32).T
+        else:
+            v, u = (texcoords * (cw, ch) + 0.5).astype(np.uint32).T
+        # clip texcoords to image
+        np.clip(u, 0, ch-1, out=u)
+        np.clip(v, 0, cw-1, out=v)
+
+        # perform uv-mapping
+        out[i[m], j[m]] = color[u[m], v[m]]
+
+
+    def getPointCloudImage(self,colorImage,colorFrame,depthFrame,pointCloudImage):
+        depth_colormap = np.asanyarray(
+            self.colorizer.colorize(depthFrame).get_data())
+        if self.state.color:
+            mapped_frame, color_source = colorFrame, colorImage
+        else:
+            mapped_frame, color_source = depthFrame, depth_colormap
+        points = self.pc.calculate(depthFrame)
+        self.pc.map_to(mapped_frame)
+
+        # Pointcloud data to arrays
+        v, t = points.get_vertices(), points.get_texture_coordinates()
+        verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+        texcoords = np.asanyarray(t).view(np.float32).reshape(-1, 2)  # uv
+
+        now = time.time()
+        self.pointCloudImage.fill(0)
+        self.grid(pointCloudImage, (0, 0.5, 1), size=1, n=10)
+        self.frustum(pointCloudImage, self.depth_intrinsics)
+        self.axes(pointCloudImage, self.view([0, 0, 0]), self.state.rotation, size=0.1, thickness=1)
+
+        if not self.state.scale or pointCloudImage.shape[:2] == (self.h, self.w):
+            self.pointcloud(pointCloudImage, verts, texcoords, color_source)
+        else:
+            tmp = np.zeros((h, w, 3), dtype=np.uint8)
+            self.pointcloud(tmp, verts, texcoords, color_source)
+            tmp = cv2.resize(
+                tmp, pointCloudImage.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+            np.putmask(pointCloudImage, tmp > 0, tmp)
+
+        if any(self.state.mouse_btns):
+            axes(pointCloudImage, view(state.pivot), state.rotation, thickness=4)
+
+        dt = time.time() - now
+        return pointCloudImage
+
 
     def displayLoop(self):
         try:
@@ -172,18 +349,9 @@ class Display():
 
                 depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-                #threshold_img = self.preProcessing(bg_removed)
-                #contours, hierarchy = cv2.findContours(threshold_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-                #cv2.imshow('threshold', threshold_img)
-                #cv2.imshow('threshold', bg_removed)
+                self.pointCloudImage = self.getPointCloudImage(color_image ,color_frame ,aligned_depth_frame, self.pointCloudImage)
+                self.display(color_image, bg_removed, depth_colormap, self.pointCloudImage)
 
-                self.display(color_image, bg_removed, depth_colormap)
-
-                # Render images
-                #cv2.drawContours(color_image, contours, -1, (0,255,0), 3)
-                #images = np.hstack((color_image, depth_colormap))
-                #cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-                #cv2.imshow('Align Example', images)
                 key = cv2.waitKey(1)
                 # Press esc or 'q' to close the image window
                 if key & 0xFF == ord('q') or key == 27:
